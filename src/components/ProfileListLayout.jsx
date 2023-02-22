8e8620e13bf932f9103a1d26dc7cc9683522aa61
suyog <suyograjenagawade@gmail.com>
2023-02-22 21:28:23 +0530

    all done

--- src/components/ProfileListLayout.jsx
+++ src/components/ProfileListLayout.jsx
@@ -1,72 +1,69 @@
-import React from "react";
+import { Box, LinearProgress } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function ProfileListLayout() {
+  const [product, setProduct] = useState([]);
+  const [fetch, setFetch] = useState(true);
+  useEffect(() => {
+    axios
+      .get(
+        "https://techcrunch.com/wp-json/wp/v2/posts?per_page=20&context=embed"
+      )
+      .then((data) => setProduct(data.data), setFetch(false));
+  }, []);
+  console.log("@SN ", product);
+  console.log("@SN ", fetch);
   return (
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-      <div class="col">
-        <div class="card h-100">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp"
-            class="card-img-top"
-            alt="Hollywood Sign on The Hill"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-          </div>
-        </div>
+    <>
+      {fetch && (
+        <Box sx={{ width: "100%" }}>
+          <LinearProgress />
+        </Box>
+      )}
+      <div
+        className="row row-cols-1 row-cols-md-4 g-4"
+        style={{ padding: "50px" }}
+      >
+        {product.map((item, index) => {
+          return (
+            <div className="col" key={index}>
+              <div className="card h-100">
+                <div
+                  className="bg-image hover-overlay hover-zoom ripple rounded"
+                  data-mdb-ripple-color="light"
+                >
+                  <img
+                    src={item.jetpack_featured_media_url}
+                    className="w-100"
+                    alt="Blue Jeans Jacket"
+                  />
+                  <a href="#!">
+                    <div
+                      className="mask"
+                      style={{
+                        backgroundColor: "rgba(251, 251, 251, 0.2)",
+                      }}
+                    ></div>
+                  </a>
+                </div>
+
+                <div className="card-body">
+                  <h5 className="card-title">{item.primary_category.name}</h5>
+                  <button type="button" class="btn btn-success">
+                    Add to cart
+                  </button>
+                  <p>{}</p>
+                  <p className="card-text">
+                    {item.primary_category.description}
+                  </p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
-      <div class="col">
-        <div class="card h-100">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp"
-            class="card-img-top"
-            alt="Palm Springs Road"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a short card.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp"
-            class="card-img-top"
-            alt="Los Angeles Skyscrapers"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp"
-            class="card-img-top"
-            alt="Skyscrapers"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
